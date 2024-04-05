213e1af9865b52917ec7b4b89d9a310e85f1117f
YznH1 <yazanbaseem.001@gmail.com>
2024-04-05 11:38:12 +0100

    added another peakdetect unit test

--- src/unitTest_PeakDetect.cpp
+++ src/unitTest_PeakDetect.cpp
@@ -17,16 +17,40 @@
 
 BOOST_AUTO_TEST_SUITE(PeakDetectTests)
 
-BOOST_AUTO_TEST_CASE(SineWavePeakDetection) 
+BOOST_AUTO_TEST_CASE(SuccessSineWavePeakDetection) 
 {
     
-
-
+    sensorTest::setData(1.2, false) // creates a sine wave with amplitude 1000
+    sensorTest::getData() //returns test data
 
     // Simulate feeding data to peak detection
     for (int32_t data : testData) 
     {
-        bool isPeak = sensor::peakDetect(data);
+        bool isPeak = sensorTest::peakDetect(data);
+
+        
+
+        if (isPeak) 
+        {
+            // Count peaks, implement verification logic here
+            int numPeaksDetected++;  
+            
+        }
+         
+    }
+    return numPeaksDetected;
+}BOOST_CHECK_EQUAL(numPeaksDetected, 3); //3 beats only since it counts two peaks as a single beat of the heart
+
+BOOST_AUTO_TEST_CASE(FailedflatwavePeakDetection) 
+{
+
+    // Simulate feeding data to peak detection
+    sensorTest::setData(1.2, true) // creates a flat wave with amplitude 0
+    sensorTest::getData() //returns test data
+    
+    for (int32_t data : testData) 
+    {
+        bool isPeak = sensorTest::::peakDetect(data);
 
         
 
