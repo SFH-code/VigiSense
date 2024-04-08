6e235b87b1b687f1233a98ef4eabaca0f9507fc7
2468513H <2468513H@student.gla.ac.uk>
2024-04-08 22:08:49 +0100

    added table framework (havent filled values)

---
+++ src/complexDiagnosis.cpp
@@ -0,0 +1,13 @@
+#include <unordered_map>
+#include <string>
+
+const std::unordered_map<std::string, std::string> diagnosisTable { { "HRBSPL", "" }
+                                               , { "HRBSPM", "" }
+                                               , { "HRBSPH", "" }
+                                               , { "HRNSPL", "" }
+                                               , { "HRNSPM", "" }
+                                               , { "HRNSPH", "" }
+                                               , { "HRTSPL", "Respiratory difficulties (pneumonia, severe asthma, COPD), pulmonary " }
+                                               , { "HRTSPM", "" }
+                                               , { "HRTSPH", "" }
+                                               };
