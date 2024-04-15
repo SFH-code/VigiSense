28e0a4a9109f77768070634279dd1e5db53ad70c
2468513H <77503201+2468513H@users.noreply.github.com>
2024-04-15 14:27:51 +0100

    Update HRTracker.h

--- src/HRTracker.h
+++ src/HRTracker.h
@@ -15,15 +15,15 @@ class HRTracker:public diagnosisInterface {
 	      testParent* _s;
 
         int lastVal=0;
-    protected:
-        sensor* _s; 
-        bool threadRunning = false;
-        void pingThread();
-        // define symptom table here
+// define symptom table here
 		std::vector<symptomRange> symptomRanges {
 			{40,60,"Bradycardia"},
 			{60,100,"Normal resting heart rate"},
 			{100,120,"Tachyacardia"}};
+    protected:
+        bool threadRunning = false;
+        void pingThread();
+        
 
 
 };
