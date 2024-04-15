f6e67c1f6dc870266873f88a5405742851f82daf
YznH1 <yazanbaseem.001@gmail.com>
2024-04-15 22:39:53 +0100

    Updated symptom ranges

--- src/SPO2Tracker.h
+++ src/SPO2Tracker.h
@@ -19,8 +19,8 @@ class SPO2Tracker:public diagnosisInterface {
         void pingThread();
         // define symptom table here
 		std::vector<symptomRange> symptomRanges {
-			{0,88,"Critically Low Oxygen concentration"},
-			{88,92,"Concerningly Low Oxygen Concentration"},
-			{92,100,"Healthy Oxygen Concentration"}};
+			{88,92,"Critically Low Oxygen concentration"},
+			{92,95,"Concerningly Low Oxygen Concentration"},
+			{95,100,"Healthy Oxygen Concentration"}};
 
 };
